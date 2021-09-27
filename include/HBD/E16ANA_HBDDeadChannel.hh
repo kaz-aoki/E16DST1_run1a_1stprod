9fef18461ecea9fe9fffe6255f36695e379adba4
Koki Kanno <kkanno@ccjbox3.riken.jp>
2021-09-27 15:01:03 +0900

    add E16ANA_HBDDeadChannel

---
+++ include/HBD/E16ANA_HBDDeadChannel.hh
@@ -0,0 +1,37 @@
+#ifndef E16ANA_HBDDeadChannel_h
+#define E16ANA_HBDDeadChannel_h
+
+#include "E16DST_DST0.hh"
+#include "E16ANA_HBDConstant.hh"
+
+#include <vector>
+
+class E16ANA_HBDDeadChannel {
+  
+public:
+  E16ANA_HBDDeadChannel();
+  ~E16ANA_HBDDeadChannel();
+  bool ReadFile(const char* filename);
+  bool ReadDeadChannelData(const int runID);
+  bool IsOK(const int module_id, const int pad_id);
+  bool IsDead(const int module_id, const int pad_id);
+  bool IsNoisy(const int module_id, const int pad_id);
+  bool IsHot(const int module_id, const int pad_id);
+  int Status(const int module_id, const int pad_id);
+  void GetOKChannels(const int module_id, std::vector<int> &ok_chs);
+  
+private:
+  enum {
+    n_modules = E16DST_Constant::NModules,
+    n_pads = HBD_Module_Constant::n_pads,
+    ok_flag = 0,
+    dead_flag = 1,
+    noisy_flag = 2,
+    hot_flag = 3
+  };
+  int status[n_modules][n_pads];
+  void Init();
+ 
+};
+
+#endif // E16ANA_HBDDeadChannel_h
