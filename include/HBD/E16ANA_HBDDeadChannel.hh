2dbb5d31bcd356c1cb9a4147bf520a5f8526bd44
Koki Kanno <kkanno@ccjbox6.riken.jp>
2022-02-24 22:21:17 +0900

    HBD dead triggertile added. use: bool E16ANA_HBDDeadChannel::IsOKTile(int module_id, int triggertile_id)

--- include/HBD/E16ANA_HBDDeadChannel.hh
+++ include/HBD/E16ANA_HBDDeadChannel.hh
@@ -20,16 +20,26 @@ public:
   int Status(const int module_id, const int pad_id);
   void GetOKChannels(const int module_id, std::vector<int> &ok_chs);
   
+  bool IsOKTile(const int module_id, const int tile_id);
+  bool IsDeadTile(const int module_id, const int tile_id);
+  bool IsNoisyTile(const int module_id, const int tile_id);
+  bool IsHotTile(const int module_id, const int tile_id);
+  int StatusTile(const int module_id, const int tile_id);
+  void GetOKChannelsTile(const int module_id, std::vector<int> &ok_chs);
+
 private:
   enum {
     n_modules = E16DST_Constant::NModules,
     n_pads = HBD_Module_Constant::n_pads,
+    n_tiles = HBD_Module_Constant::n_tiles,
     ok_flag = 0,
     dead_flag = 1,
     noisy_flag = 2,
     hot_flag = 3
   };
+  
   int status[n_modules][n_pads];
+  int status_tile[n_modules][n_tiles];
   void Init();
  
 };
