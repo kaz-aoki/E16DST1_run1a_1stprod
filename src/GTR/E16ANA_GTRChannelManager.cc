533eec9fa4fd36598657de0aa0cb594582bc4ab2
tomomoki <mtomoki@post.kek.jp>
2023-01-16 15:55:50 +0900

    edited code around gtr dead ch map

--- src/GTR/E16ANA_GTRChannelManager.cc
+++ src/GTR/E16ANA_GTRChannelManager.cc
@@ -51,11 +51,11 @@ int E16ANA_GTRChannelManager::ConvLocalXToGEMch(const int n_gem_strip_x, const d
 	}
 	else if(n_gem_strip_x == 4){//200GEM
 		double conv_lx = lx_mm + 100;
-		gem_ch = conv_lx/50;
+		gem_ch =  (int)conv_lx/50;
 	}
 	else if(n_gem_strip_x == 12){//300GEM
-		double conv_lx = lx_mm + 150;
-		gem_ch  = conv_lx/25; 
+		double conv_lx = lx_mm + 150.6;
+		gem_ch  = (int)conv_lx/25.1; 
 	}
 	return gem_ch;
 }
@@ -67,70 +67,91 @@ int E16ANA_GTRChannelManager::ConvLocalYToGEMch(const int n_gem_strip_y, const d
 	}
 	else if(n_gem_strip_y == 4){//200GEM
 		double conv_ly = ly_mm + 100;
-		gem_ch = conv_ly/50;
+		int    div = conv_ly/50;
+		gem_ch = n_gem_strip_y - 1-   div;
 	}
 	else if(n_gem_strip_y == 24){//300GEM
-		double conv_ly = ly_mm + 150;
-		gem_ch  = conv_ly/12.5; 
+		double conv_ly = ly_mm + 150.6;
+		int    div = conv_ly/12.55;
+		gem_ch  = n_gem_strip_y - 1 - div; 
+//		if(gem_ch > 20){
+//			std::cout << "conv " << conv_ly << std::endl;
+//			std::cout << "n_gem_s " << n_gem_strip_y << std::endl;
+//			std::cout << "divi " << div << std::endl;
+//			std::cout << "gem_ch " << gem_ch << std::endl;
+//		}
 	}
 	return gem_ch;
 }
 
 int E16ANA_GTRChannelManager::ConvLocalXToAPVch(const int gtr_size, const double lx_mm){//
-	int n_strips_100 = E16DST_DST1Constant::nstrips_100x;
-	int n_strips_200 = E16DST_DST1Constant::nstrips_200x;
-	int n_strips_300 = E16DST_DST1Constant::nstrips_300x;
-	int n_strips = -1;
-	if(gtr_size == 0) {n_strips = n_strips_100;}
-	else if(gtr_size == 1) {n_strips = n_strips_200;}
-	else if(gtr_size == 2) {n_strips = n_strips_200;}
-	else {return -1;}
+	int n_strips[E16DST_DST1Constant::n_gtr_layers] = {E16DST_DST1Constant::nstrips_100x, E16DST_DST1Constant::nstrips_200x, E16DST_DST1Constant::nstrips_300x};
     int apv_ch = -1;
-    if(signbit(lx_mm)){//MINUS
-		apv_ch = (n_strips/2 - 1) + (int)lx_mm/E16DST_DST1Constant::gtr_strip_pitch_x;
-	}
-    else {
-		apv_ch = n_strips/2  + (int)lx_mm/E16DST_DST1Constant::gtr_strip_pitch_x;
-	}
+	double pitch = E16DST_DST1Constant::gtr_strip_pitch_x;
+//    if(signbit(lx_mm)){//MINUS
+//		apv_ch = (n_strips[gtr_size]/2 - 1) + (int)lx_mm/E16DST_DST1Constant::gtr_strip_pitch_x;
+//	}
+//  else {
+//		apv_ch =  n_strips[gtr_size]/2  + (int)lx_mm/E16DST_DST1Constant::gtr_strip_pitch_x;
+//			}
+    apv_ch = (lx_mm + pitch *  n_strips[gtr_size]/2) / pitch;
 	return apv_ch;
 }
 
-int E16ANA_GTRChannelManager::ConvLocalYToAPVch(const int gtr_size, const double lx_mm, const double ly_mm ){//
-	int n_strips_100 = E16DST_DST1Constant::nstrips_100y;
-	int n_strips_200 = E16DST_DST1Constant::nstrips_200y;
-	int n_strips_300 = E16DST_DST1Constant::nstrips_300y;
-	int n_strips = -1;
-	if(gtr_size == 0) {n_strips = n_strips_100;}
-	else if(gtr_size == 1) {n_strips = n_strips_200;}
-	else if(gtr_size == 2) {n_strips = n_strips_200;}
-	else {return -1;}
-    int apv_ch = -1;
-    if(gtr_size == 0){
-		if(IsYa(lx_mm)){//Ya
-		    if(signbit(ly_mm)){//MINUS
-				apv_ch = (n_strips/2 - 1) + (int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
-			}
-	    	else {
-				apv_ch = n_strips/2 + (int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
-			}
+int E16ANA_GTRChannelManager::ConvLocalYToAPVch(const int gtr_size, const double lx_mm, const double ly_mm ){//if it is Yb, 72 is added to strip_id, be carefull!!!
+	int n_strips[E16DST_DST1Constant::n_gtr_layers] = {E16DST_DST1Constant::nstrips_100y, E16DST_DST1Constant::nstrips_200y, E16DST_DST1Constant::nstrips_300y};
+	int apv_ch = -1;
+	double pitch = E16DST_DST1Constant::gtr_strip_pitch_y;
+	if(gtr_size ==0 ){
+		if(IsYa(lx_mm)){
+		      apv_ch =   n_strips[gtr_size] -  (ly_mm + pitch * n_strips[gtr_size]/2) / pitch;			
 		}
-		else{//Yb
-		    if(signbit(ly_mm)){//MINUS
-				apv_ch = n_strips/2  + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y  ;
-			}
-	    	else {
-				apv_ch = (n_strips/2 - 1) + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y ;
-			}
-		}			
-	}//GTR100
-    else{//GTR200, 300
-    if(signbit(ly_mm)){//MINUS
-		apv_ch = (n_strips/2 - 1) + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+		else{ apv_ch = 144 + ((  ly_mm - pitch * n_strips[gtr_size]/2) / pitch) ;
+		}//Yb
 	}
-    else {
-		apv_ch = n_strips/2  + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+	else {
+		      apv_ch =   n_strips[gtr_size] -  (ly_mm + pitch * n_strips[gtr_size]/2) / pitch;			
 	}
-	}
+
+
+//	int n_strips_100 = E16DST_DST1Constant::nstrips_100y;
+//	int n_strips_200 = E16DST_DST1Constant::nstrips_200y;
+//	int n_strips_300 = E16DST_DST1Constant::nstrips_300y;
+//	int n_strips = -1;
+//	if(gtr_size == 0) {n_strips = n_strips_100;}
+//	else if(gtr_size == 1) {n_strips = n_strips_200;}
+//	else if(gtr_size == 2) {n_strips = n_strips_200;}
+//	else {return -1;}
+//    int apv_ch = -1;
+//    if(gtr_size == 0){
+//		if(IsYa(lx_mm)){//Ya
+//		    if(signbit(ly_mm)){//MINUS
+//				apv_ch = (n_strips/2 - 1) + (int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+//				std::cout <<  ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y << std::endl;
+//			}
+//	    	else {
+//				apv_ch = n_strips/2 + (int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+//				std::cout <<  ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y << std::endl;
+//			}
+//		}
+//		else{//Yb
+//		    if(signbit(ly_mm)){//MINUS
+//				apv_ch = n_strips/2  + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y +72 ;
+//				std::cout <<  ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y << std::endl;
+//			}
+//	    	else {
+//				apv_ch = (n_strips/2 - 1) + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y +72 ;
+//			}
+//		}			
+//	}//GTR100
+//    else{//GTR200, 300
+//    if(signbit(ly_mm)){//MINUS
+//		apv_ch = (n_strips/2 - 1) + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+//	}
+//    else {
+//		apv_ch = n_strips/2  + -1*(int)ly_mm/E16DST_DST1Constant::gtr_strip_pitch_y;
+//	}
+//	}
 	return apv_ch;
 }
 
