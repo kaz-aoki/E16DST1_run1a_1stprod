f1bb8fad0d475cee42a4d45ca52a7ab1bf190486
snakasuga <nakasuga.satomi.33a@st.kyoto-u.ac.jp>
2023-06-19 16:32:44 +0900

    230619 from ccj, update in Gammaana 2

---
+++ usr/tmp/Gammaana_CodeCheck.cc
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <TROOT.h>
+#include <TH1.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TRandom.h>
+// #include <boost/program_options.hpp>
+
+#include "E16ANA_CalibDBManager.hh"
+#include "E16ANA_GTRcalib.hh"
+#include "E16ANA_TriggerCalib.hh"
+#include "E16DST_DST0.hh"
+#include "E16DST_DST1.hh"
+#include "E16DST_DST1DetectorFactory.hh"
+#include "E16DST_DST1DefaultFilePath.hh"
+#include "E16ANA_LGBasic.hh"
+#include "E16ANA_LGWaveform.hh"
+#include "E16ANA_LGConstant.hh"
+#include "E16ANA_LGDeadChannel.hh"
+#include "E16ANA_LGClustering.hh"
+#include "E16ANA_LGCheckHist.hh"
+#include "E16ANA_Massw2Gamma.hh"
+#include "E16ANA_GammaAnalyzer.hh"
+#include "E16DST_Constant.hh"
+
+using namespace std;
+// namespace  bpo = boost::program_options;
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    cerr << "Invalid argc: " << argc << endl;
+    cerr << "./bin [input.txt] [output.root] [output.pdf]" << endl;
+    return -1;
+  }
+  auto in_file_name  = argv[1];
+  auto out_file_name = argv[2];
+  auto out_pdf_name = argv[3];
+
+  // std::ifstream fin("/ccj/u/nakasuga/work/dst1ana/track/etajamout.txt");
+  std::ifstream fin(in_file_name);
+  TFile* fout = new TFile(out_file_name,"recreate");
+
+  auto *analyzer = new E16ANA_GammaAnalyzer();
+
+  int n_event = 0;
+  while (!fin.eof()) {
+    if (n_event % 1000 == 0) {
+      cout << "Number of event: " << n_event << endl;
+    }
+
+    double gx1, gy1, gz1, gx2, gy2, gz2, ene1, ene2;
+    fin >> gx1 >> gy1 >> gz1 >> ene1 >> gx2 >> gy2 >> gz2 >> ene2;
+
+    analyzer->ClearHit0();
+    analyzer->SetHit0(0, 0, 0, 0., 0., 0., gx1, gy1, gz1, ene1/analyzer->GetParam(), 0., 0);
+    if( analyzer->Hit0isInvalid() ) continue;
+
+    analyzer->ClearHit1();
+    analyzer->SetHit1(0, 0, 0, 1000., 1000., 1000., gx2, gy2, gz2, ene2/analyzer->GetParam(), 0., 0);
+    if( analyzer->Hit1isInvalid() ) continue;
+    if( analyzer->HitsareInvalid() ) continue;
+
+    analyzer->ClearBranch();
+    analyzer->FillHit0();
+    analyzer->FillHit1();
+    //calc im
+    analyzer->FillForeHist();
+    //calc mix im
+    analyzer->FillTree();
+
+    ++n_event;
+
+  }
+
+  TString fsout = Form("%s",out_pdf_name);
+  TCanvas* c = new TCanvas("c","c",1000,700);
+  c->SaveAs(fsout+"[","pdf");
+  analyzer->Draw(fsout,c);
+  c->SaveAs(fsout+"]","pdf");
+
+  fout->Write();
+  fout->Close();
+
+  return 0;
+}
