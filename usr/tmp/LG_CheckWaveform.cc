ccd2552f5ff5d88af8f25f07a1898175c032e54a
snakasuga <nakasuga.satomi.33a@st.kyoto-u.ac.jp>
2023-07-19 19:27:58 +0900

    230719 form evb02, CMakeLists for evb, w/bug

---
+++ usr/tmp/LG_CheckWaveform.cc
@@ -0,0 +1,36 @@
+#include "E16ANA_LGOnlineAnalyzer.hh"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TLine.h>
+
+#include <TH1.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "TROOT.h"
+
+int main(int argc, char* argv[]) {
+
+   if ( argc!=5 ) {
+     std::cerr << "./bin [run No.] [runxxxxxx.root] [outfile.pdf] [maxevent]" << std::endl;
+     return -1;
+   }
+   auto run_id = stoi(argv[1]);
+   auto infile = argv[2];
+   auto outfile = argv[3];
+   auto maxevent = stoi(argv[4]);
+
+   auto in_chain = new TChain("tree", "tree");
+
+   in_chain->Add(Form("%s",infile));
+
+   E16ANA_LGOnlineAnalyzer* t = new E16ANA_LGOnlineAnalyzer(in_chain);
+   t->CheckWaveform(run_id, outfile, maxevent);
+  
+   delete in_chain;
+   return 0;
+
+}
