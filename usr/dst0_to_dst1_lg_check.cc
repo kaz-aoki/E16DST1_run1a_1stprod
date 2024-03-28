14c972afa88a6fe8899871ef46332bf546d718ca
snakasuga <nakasuga.satomi.33a@st.kyoto-u.ac.jp>
2024-03-28 20:03:55 +0900

    update E16DST etc.

--- usr/dst0_to_dst1_lg_check.cc
+++ usr/dst0_to_dst1_lg_check.cc
@@ -23,8 +23,8 @@
 using namespace std;
 // namespace  bpo = boost::program_options;
 
-// #define WF_ON
-// #define TRG_ON
+#define WF_ON
+#define TRG_ON
 #define LGDST1_ON
 
 int main(int argc, char* argv[]) {
