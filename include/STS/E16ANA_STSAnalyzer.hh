2d6c6ad3205b8b0fdbedc438bf658e3d63c553b6
Kazuya Aoki <kazuya.aoki@kek.jp>
2024-05-21 02:26:07 +0900

    cluster variable calculation.

--- include/STS/E16ANA_STSAnalyzer.hh
+++ include/STS/E16ANA_STSAnalyzer.hh
@@ -3,6 +3,8 @@
 
 
 #include "E16DST_DST1.hh"
+#include <vector>
+#include <numeric>
 
 struct E16ANA_STSClusterParam {
   int adc_thresh = 0;
@@ -13,6 +15,10 @@ struct E16ANA_STSClusterParam {
 class E16ANA_STSCluster{
 public:
   std::vector<E16DST_DST1STSHit> hits;
+  int adc();
+  double tdc();
+  double cog();
+  double timing(E16ANA_STSClusterParam& param);
   
   void show() {
     std::cout << "STSCluster:" << std::endl;
@@ -20,6 +26,7 @@ public:
       std::cout << "mod=" << iter->ModuleId() << ", PN=" << iter->PN() << ", Strip=" << iter->StripId() <<
 	", tdc=" << iter->TDC() << ", adc=" << iter->ADC() << std::endl;
     }
+    std::cout << "  adc_sum=" << adc() << ",  tdc=" << tdc() << " cog=" << cog() << std::endl;
   }
 };
 
@@ -29,11 +36,13 @@ public:
   E16ANA_STSAnalyzer() {;}
   void sort(std::vector<E16DST_DST1STSHit>& hits1);
   void clusterize(std::vector<E16DST_DST1STSHit>& hits1, std::vector<E16DST_DST1STSCluster>& clusters1);
+  void fill_dst1(std::vector<E16DST_DST1STSCluster>& clusters1);
   
   void set_param(E16ANA_STSClusterParam _param) { param = _param; }
   E16ANA_STSClusterParam get_param() { return param; }
 private:
   E16ANA_STSClusterParam param;
+  std::vector<E16ANA_STSCluster> vec_cluster;
 
 };
 
