f3b5e08787b088636275a1472057ff02f2c67ed5
tomomoki <mtomoki@post.kek.jp>
2024-06-19 02:06:19 +0900

    TChain applied

--- usr/tmp_murakami/draw_HistStraight.cc
+++ usr/tmp_murakami/draw_HistStraight.cc
@@ -1,8 +1,9 @@
 #include "E16DSTN_ReStraightV2.hh"
+#include <fstream>
 
 int main (int argc, char** argv) {
 	if(argc != 7){
-		std::cout << "./bin/~~ [input.root] [output.root] [start_ev] [end_ev] [residual_layer] [pdf_name]" << std::endl;
+		std::cout << "./bin/~~ [input_list.txt] [output.root] [start_ev] [end_ev] [residual_layer] [pdf_name]" << std::endl;
 		return 0;	
 	}
    std::string in_file   = argv[1];
@@ -11,17 +12,32 @@ int main (int argc, char** argv) {
 	int         end_event  = stoi(argv[4]);
 	int         residual_layer      = stoi(argv[5]);
 	TString pdf_name      = argv[6];
-//   const char *dirname = "/work22/E16/mtomoki/run0c_wire/230929-wire-v1/afterkill_root/";
-  TFile *fin = new TFile(in_file.c_str());
-  TTree *tree = (TTree*)fin->Get("tree");
+
+	TChain *chain = new TChain("tree");
+	std::string line;
+	std::ifstream infile(in_file);
+   if (!infile.is_open()) {
+      std::cerr << "Error: Could not open "  << in_file << std::endl;
+      return 0;
+   }
+   while (std::getline(infile, line)) {
+           chain->Add(line.c_str());
+   }
+   infile.close();
+   std::cout << "Number of files in TChain: " << chain->GetListOfFiles()->GetEntries() << std::endl;
+
+
+//   TFile *fin = new TFile(in_file.c_str());
+//   TTree *tree = (TTree*)fin->Get("tree");
 
 //	TFile *fin           = new TFile(in_file.c_str());
 //	TTree *tree          = (TTree*)fin->Get("outtree");
 	std::vector<TVector3> dummy;
-	E16DSTN_ReStraightV2 *rt = new E16DSTN_ReStraightV2(tree, out_file.c_str(), nullptr, nullptr, nullptr, dummy);//initialize
+//	E16DSTN_ReStraightV2 *rt = new E16DSTN_ReStraightV2(tree, out_file.c_str(), nullptr, nullptr, nullptr, dummy);//initialize
+	E16DSTN_ReStraightV2 *rt = new E16DSTN_ReStraightV2(chain, out_file.c_str(), nullptr, nullptr, nullptr, dummy);//initialize
 	int print_cycle      = 1000;
    
-	rt->DrawHist(tree, start_event, end_event,  print_cycle, residual_layer, pdf_name);
+	rt->DrawHist(chain, start_event, end_event,  print_cycle, residual_layer, pdf_name);
 	
 	return 0;	
 }
