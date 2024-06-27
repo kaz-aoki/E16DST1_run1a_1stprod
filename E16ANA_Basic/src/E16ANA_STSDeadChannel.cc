d76b86a7c118c4a6d7a55d31db4f59782e2dff3b
0118yama <yamadarento13@gmail.com>
2024-06-27 11:48:19 +0900

    add deadch header & check file of dst0 to dst1

---
+++ E16ANA_Basic/src/E16ANA_STSDeadChannel.cc
@@ -0,0 +1,100 @@
+//E16ANA_STSDeadChannel.cc 240626 by R.Yamada
+//    Last modified at <2024-06-26 20:33:31 >
+//
+
+#include "E16ANA_ErrorMessage.hh"
+#include "E16ANA_STSDeadChannel.hh"
+#include "E16ANA_CalibDBManager.hh"
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+E16ANA_STSDeadChannel::E16ANA_STSDeadChannel()
+{
+    Init();
+}
+
+E16ANA_STSDeadChannel::~E16ANA_STSDeadChannel()
+{
+}
+
+bool E16ANA_STSDeadChannel::ReadFile(const char *filename){
+    Init();
+
+    std::ifstream fin(filename);
+    if (!fin) {
+        std::cerr << __func__ << " invalid dead channel filename: " << filename << std::endl;
+        return false;
+    }
+
+    std::string buf_line;
+    while (std::getline(fin, buf_line)) {
+        if (buf_line.empty() || buf_line[0] == '#') continue; //skip comment or empty line
+
+        std::stringstream ss(buf_line);
+        uint16_t module_id, pol_id, ch_id;
+        int ch_status;
+        ss >> module_id >> pol_id >> ch_id >> ch_status;
+
+        int k_module_id = ConvertModuleID(module_id);
+        if (k_module_id != -1) {
+            status[k_module_id][pol_id][ch_id] = ch_status;
+        }
+    }
+    return true;
+}
+//============================================================
+bool E16ANA_STSDeadChannel::ReadDeadChannelData(const int runID){
+    E16ANA_CalibDBManager &calib = E16ANA_CalibDBManager::Instance();
+    std::string sts_dead_channel_file = calib.CalibFileName("STS-deadch", runID);
+    this->ReadFile(sts_dead_channel_file.c_str());
+    return true;
+}
+
+//============================================================
+bool E16ANA_STSDeadChannel::IsOK(const uint16_t module_id, const uint16_t pol_id, const uint16_t ch_id) const {
+  int k_module_id = ConvertModuleID(module_id);
+  return status[k_module_id][pol_id][ch_id] == 0;
+}
+
+bool E16ANA_STSDeadChannel::IsDead(const uint16_t module_id, const uint16_t pol_id, const uint16_t ch_id) const {
+  int k_module_id = ConvertModuleID(module_id);
+  return status[k_module_id][pol_id][ch_id] == 1;
+}
+
+bool E16ANA_STSDeadChannel::IsNoisy(const uint16_t module_id, const uint16_t pol_id, const uint16_t ch_id) const {
+  int k_module_id = ConvertModuleID(module_id);
+  return status[k_module_id][pol_id][ch_id] == 2;
+}
+
+bool E16ANA_STSDeadChannel::IsHot(const uint16_t module_id, const uint16_t pol_id, const uint16_t ch_id) const {
+  int k_module_id = ConvertModuleID(module_id);
+  return status[k_module_id][pol_id][ch_id] == 3;
+}
+
+//==============================================================
+int E16ANA_STSDeadChannel::ConvertModuleID(uint16_t module_id) const {
+    std::vector<uint16_t> k_module_ids = {101, 102, 103, 104, 106, 107, 108, 109, 206, 207};
+
+    for (int i = 0; i < k_module_ids.size(); ++i) {
+        if (k_module_ids[i] == module_id) {
+            return i;
+        }
+    }
+    std::cerr << "Invalid module_id: " << module_id << std::endl;
+    return -1;
+}
+
+//===============================================================
+void E16ANA_STSDeadChannel::Init(){
+    for (uint16_t i = 0; i < n_modules; ++i) {
+        for (uint16_t j = 0; j < 2; ++j) {
+            for (uint16_t k = 0; k < n_channels; ++k) {
+                status[i][j][k] = 0;// Set default status to 0
+            }
+        }
+    }
+}
